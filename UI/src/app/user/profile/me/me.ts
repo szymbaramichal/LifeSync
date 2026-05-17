fd44455fe2e696eafdb7300f3634a0f2f2b50660
szymbaramichal <szymbaramichal@gmail.com>
2026-05-17 18:43:58 +0200

    FE: basic logout feature

--- UI/src/app/user/profile/me/me.ts
+++ UI/src/app/user/profile/me/me.ts
@@ -1,13 +1,19 @@
 import { afterNextRender, Component, inject, input, signal } from '@angular/core';
 import { MeDto } from '../profile.models';
+import { MatCard } from "@angular/material/card";
+import { MatButton } from '@angular/material/button';
+import { AuthService } from '../../../auth/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [MatCard, MatButton],
   templateUrl: './me.html',
   styleUrl: './me.css',
 })
 export class Me {
+  authService = inject(AuthService);
+  router = inject(Router);
   me = input.required<MeDto>();
   username = '';
 
@@ -15,5 +21,10 @@ export class Me {
     afterNextRender(() => {
       this.username = this.me().username;
     });
+  }
+
+  async onLogout() {
+    await this.authService.logout();
+    this.router.navigate(['auth']);
   }
 }
